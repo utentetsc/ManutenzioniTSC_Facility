f367e9b007705c31cb1bc13a9c384ccbf58633ec
utentetsc <utente.tsc@gmail.com>
2026-09-16 16:27:59 +0200

    Create layout.tsx

---
+++ layout.tsx
@@ -0,0 +1,15 @@
+export const metadata = {
+  title: "The Space Maintenance Manager"
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="it">
+      <body>{children}</body>
+    </html>
+  );
+}
